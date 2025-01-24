03125de11480bffe870c13d3594513f221fd1411
Mustafa hilmi oruç <130426180+mh0ruc@users.noreply.github.com>
2025-01-25 01:39:09 +0300

    Update header.component.ts

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -6,10 +6,16 @@ import { RouterModule } from '@angular/router';
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.css'],
-  imports:[RouterModule,CommonModule]
+  imports: [CommonModule, RouterModule]  // CommonModule burada ekli olmalı
 })
 export class HeaderComponent {
   categories: string[] = [
     "Business", "Entertainment", "General", "Health", "Science", "Sports", "Technology"
   ];
+  
+  isMenuOpen = false;
+
+  toggleMenu() {
+    this.isMenuOpen = !this.isMenuOpen;
+  }
 }
